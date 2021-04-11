01e0d1b3223f90beddbd6f4b17c82a20b1a80464
bilalkirkgul <61188245+bilalkirkgul@users.noreply.github.com>
2021-04-11 15:37:22 +0300

    FragmanVM New Model Created

---
+++ MovieAndFragman.UI.CoreMVC/Models/FragmanVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieAndFragman.UI.CoreMVC.Models
+{
+    public class FragmanVM
+    {
+    }
+}
