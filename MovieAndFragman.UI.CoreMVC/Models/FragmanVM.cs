72e8d586a37cf7c9e54f25d34501f2f93e45d889
afyavuz53 <76837995+afyavuz53@users.noreply.github.com>
2021-04-11 15:39:04 +0300

    FragmanvVM dolduruldu.

--- MovieAndFragman.UI.CoreMVC/Models/FragmanVM.cs
+++ MovieAndFragman.UI.CoreMVC/Models/FragmanVM.cs
@@ -7,5 +7,13 @@ namespace MovieAndFragman.UI.CoreMVC.Models
 {
     public class FragmanVM
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Poster { get; set; }//afiş
+        public int UserID { get; set; }
+        public string InitialRelease { get; set; }// ilk gösterim
+        public string Director { get; set; } //Yönetmen
+        public string Production { get; set; } // Yapımcı
+        public float IMDB { get; set; }
     }
 }
