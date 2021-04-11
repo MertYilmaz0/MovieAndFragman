a5beae1d290b270748cdda598cbde490fe94e43c
afyavuz53 <76837995+afyavuz53@users.noreply.github.com>
2021-04-11 14:09:46 +0300

    Api Key entegresi

---
+++ MovieAndFragman.Service.WebAPI/Controllers/FragmanController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieAndFragman.Service.WebAPI.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieAndFragman.Service.WebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [ApiKey]
+    public class FragmanController : ControllerBase
+    {
+    }
+}
