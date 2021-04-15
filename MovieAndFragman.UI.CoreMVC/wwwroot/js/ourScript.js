44cabec2fde240ba91f85c8030119369a38057f6
afyavuz53 <76837995+afyavuz53@users.noreply.github.com>
2021-04-15 17:40:47 +0300

    Loader eklendi

---
+++ MovieAndFragman.UI.CoreMVC/wwwroot/js/ourScript.js
@@ -0,0 +1,12 @@
+﻿(function ($) {
+    'use strict';
+
+    var browserWindow = $(window);
+
+    browserWindow.on('load', function () {
+        $('.preloader').fadeOut(10000, function () {
+            $(this).remove();
+        });
+    });
+
+})(jQuery);
