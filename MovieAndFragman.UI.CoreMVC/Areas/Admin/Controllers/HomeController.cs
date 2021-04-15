6f0575639c8e91f5ff8bfae7d2b789229cf67b8f
bilalkirkgul <61188245+bilalkirkgul@users.noreply.github.com>
2021-04-16 00:24:43 +0300

    Areas-Admin

---
+++ MovieAndFragman.UI.CoreMVC/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieAndFragman.UI.CoreMVC.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Area("Admin")]
+        [Authorize(Roles = "Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
