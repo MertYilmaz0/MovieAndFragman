59c1e1ca24752adbadf75e481d5137374202e48d
bilalkirkgul <61188245+bilalkirkgul@users.noreply.github.com>
2021-04-16 19:45:17 +0300

    Startup areas eklendi

--- MovieAndFragman.UI.CoreMVC/Startup.cs
+++ MovieAndFragman.UI.CoreMVC/Startup.cs
@@ -45,6 +45,13 @@ namespace MovieAndFragman.UI.CoreMVC
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "area",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+
+                    );
+
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}"
                     );
