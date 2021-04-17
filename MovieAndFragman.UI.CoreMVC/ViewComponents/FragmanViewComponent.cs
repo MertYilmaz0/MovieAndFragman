4d6853eedcb9642ee53611242633559fabb638d6
afyavuz53 <76837995+afyavuz53@users.noreply.github.com>
2021-04-17 19:34:04 +0300

    Home index düzeltildi.

---
+++ MovieAndFragman.UI.CoreMVC/ViewComponents/FragmanViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using MovieAndFragman.UI.CoreMVC.Helper;
+using MovieAndFragman.UI.CoreMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieAndFragman.UI.CoreMVC.ViewComponents
+{
+    public class FragmanViewComponent: ViewComponent
+    {
+        public ViewViewComponentResult Invoke()
+        {
+            List<FragmanVM> fragmanVM = ApiJsonHelper<FragmanVM>.GetApiEntityList("fragman/GetLastThirdFragman");
+            return View(fragmanVM);
+        }
+    }
+}
