e290b306b71f2326c8313f79d9c45c7a5f37a8d9
afyavuz53 <76837995+afyavuz53@users.noreply.github.com>
2021-04-25 14:48:53 +0300

    live and podcast added to bll

---
+++ MovieAndFragman.BLL/Concrete/LiveService.cs
@@ -0,0 +1,42 @@
+﻿using MovieAndFragman.BLL.Abstract;
+using MovieAndFragman.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieAndFragman.BLL.Concrete
+{
+    class LiveService : ILiveBLL
+    {
+        //todo: live
+        public void Delete(Live entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteById(int entityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Live Get(int entityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Live> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Live entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Live entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MovieAndFragman.BLL/Concrete/PodcastService.cs
@@ -0,0 +1,42 @@
+﻿using MovieAndFragman.BLL.Abstract;
+using MovieAndFragman.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieAndFragman.BLL.Concrete
+{
+    class PodcastService : IPodcastBLL
+    {
+        //todo: podcast
+        public void Delete(Podcast entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteById(int entityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Podcast Get(int entityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Podcast> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Podcast entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Podcast entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
