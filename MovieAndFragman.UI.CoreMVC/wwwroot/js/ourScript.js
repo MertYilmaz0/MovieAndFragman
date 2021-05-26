a1c8c98944f6cf6548cf49177b26a3860c89abd2
MertYilmaz0 <a.mertyilmaz92@gmail.com>
2021-05-26 23:22:00 +0300

    resimler

--- MovieAndFragman.UI.CoreMVC/wwwroot/js/ourScript.js
+++ MovieAndFragman.UI.CoreMVC/wwwroot/js/ourScript.js
@@ -4,7 +4,7 @@
     var browserWindow = $(window);
 
     browserWindow.on('load', function () {
-        $('.preloader').fadeOut(3000, function () {
+        $('.preloader').fadeOut(5000, function () {
             $(this).remove();
         });
     });
